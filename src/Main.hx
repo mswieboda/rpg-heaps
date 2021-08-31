c6e1fe7b3a17fab856917792dabe37c494eb3224
Matt Swieboda <mswieboda@users.noreply.github.com>
2021-08-30 22:36:31 -0500

    switch scenes

--- src/Main.hx
+++ src/Main.hx
@@ -10,12 +10,14 @@ import hxd.res.DefaultFont;
 import hxd.SceneEvents;
 
 class Main extends App {
-  // var sceneClass;
+  var scene3d : h3d.scene.Scene;
+  var scene2d : h2d.Scene;
 
-  // var menuScene : h2d.Scene;
-  // var menuText : Text;
+  var menuScene3d : h3d.scene.Scene;
+  var menuScene2d : h2d.Scene;
+  var menuText : Text;
 
-  var gameScene : h3d.scene.Scene;
+  var gameScene3d : h3d.scene.Scene;
   var gameScene2d : h2d.Scene;
   var map : WorldMap;
   var gameText : Text;
@@ -24,58 +26,79 @@ class Main extends App {
 
   override function init() {
     // menu scene
-    // menuScene = new h2d.Scene();
-    // menuText = new Text(DefaultFont.get(), menuScene);
+    menuScene3d = new h3d.scene.Scene();
+    menuScene2d = new h2d.Scene();
+    menuText = new Text(DefaultFont.get(), menuScene2d);
+    menuText.text = 'menu testing 123';
 
     // game scene
-    gameScene = new h3d.scene.Scene();
+    gameScene3d = new h3d.scene.Scene();
     gameScene2d = new h2d.Scene();
 
-    map = new WorldMap(16, 256, gameScene);
+    map = new WorldMap(16, 256, gameScene3d);
 
-    player = new Player(gameScene);
+    player = new Player(gameScene3d);
 
     player.x = 64;
     player.y = 64;
     player.z = 0.99;
 
-    camera = new Camera(gameScene);
+    camera = new Camera(gameScene3d);
 
     // add directional light
-    new DirLight(new Vector( 0.3, -0.4, -0.9), gameScene);
-    cast(gameScene.lightSystem, LightSystem).ambientLight.setColor(0x909090);
+    new DirLight(new Vector( 0.3, -0.4, -0.9), gameScene3d);
+    cast(gameScene3d.lightSystem, LightSystem).ambientLight.setColor(0x909090);
 
     // add debug text/HUD
     gameText = new Text(DefaultFont.get(), gameScene2d);
 
-    // set the initial scene
-    // sceneClass = MenuScene;
+    scene3d = menuScene3d;
+    scene2d = menuScene2d;
 
-    // setScene(menuScene);
-    setScene(gameScene);
+    s3d.addChild(scene3d);
+    s2d.addChild(scene2d);
   }
 
   override function update(dt: Float) {
-    // menuText.text = 'menu testing 123';
-
     gameText.text = 'player pos: [${player.x}, ${player.y}] drawCalls: ${engine.drawCalls}';
 
     player.update(dt);
     camera.update(dt, player);
 
-    // if(Key.isPressed(Key.TAB)) {
-    //   trace("switching scene");
-    //   sceneClass = sceneClass == "MenuScene" ? "GameScene" : "MenuScene";
-
-    //   var scene = sceneClass == "MenuScene" ? new GameScene() : new MenuScene();
-
-    //   setScene(new sceneClass(), false);
-    //   trace('switched scene $scene');
-    // }
+    if(Key.isPressed(Key.NUMBER_2)) {
+      trace(">>> switching scene to game");
+      switchScene(gameScene3d, gameScene2d);
+    } else if (Key.isPressed(Key.NUMBER_1)) {
+      trace(">>> switching scene to menu");
+      switchScene(menuScene3d, menuScene2d);
+    }
 
     if(Key.isDown(Key.ESCAPE)) {
       System.exit();
     }
+  }
+
+
+  function switchScene(scene3d : h3d.scene.Scene, scene2d : h2d.Scene) {
+    if (this.scene3d == scene3d && this.scene2d == scene2d) {
+      trace(">>> switchScene same scene, returning");
+      return;
+    }
+
+    // s3d.removeChild(scene3d);
+    // s2d.removeChild(scene2d);
+
+    this.scene3d.remove();
+    this.scene2d.remove();
+
+    this.scene3d = null;
+    this.scene2d = null;
+
+    this.scene3d = scene3d;
+    this.scene2d = scene2d;
+
+    s3d.addChild(this.scene3d);
+    s2d.addChild(this.scene2d);
   }
 
   static function main() {
